8fb6a4aeb89d10e749b8f1157b60e7832476f6f6
Yenny <Yenny.Jo@obigo.com>
2023-08-09 11:03:10 +0900

    tttttest

--- src/sum.cc
+++ src/sum.cc
@@ -1,8 +1,13 @@
 #include "../inc/sum.h"
 #include <fstream>
+#include <tuple>
 
 using namespace std;
 
 void sum(string fName){
-    fstream *ifs;
+    int x,y,z;
+    tuple<int,int,int> t = make_tuple(1,2,3);
+    tie(x,y,z) = t;
+    fstream ifs;
+    ifs.open(fName);
 }
