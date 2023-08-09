5bfde2f9579b9df0e2094c7a57cf520fdddac4dd
Yenny <Yenny.Jo@obigo.com>
2023-08-09 09:43:12 +0900

    testt

--- test/sumTest.cc
+++ test/sumTest.cc
@@ -5,6 +5,10 @@ TEST(SumTest, Test1){
     sum("hi.txt");
 }
 
+TEST(SumTest, Test2){
+    sum("hello.txt");
+}
+
 int main(int argc, char* argv[]){
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
